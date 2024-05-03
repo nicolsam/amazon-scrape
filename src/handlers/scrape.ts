679576934578ddae049f2fa75c3d544332c00db8
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2024-05-02 23:46:01 -0300

    feat: scrapping quotes and returning JSON

--- src/handlers/scrape.ts
+++ src/handlers/scrape.ts
@@ -1,5 +1,40 @@
+import axios from "axios";
 import { Request, Response } from "express";
+import jsdom from "jsdom";
+import { Quote } from "../types/quote";
 
-export function getScrapeResult(request: Request, response: Response) {
-    response.send([]);
-}
+export async function getScrapeResult(request: Request, response: Response) {
+
+    const productsList: Quote[] = [];
+
+    // Fetching data
+    const axiosResponse = await axios.get(`https://quotes.toscrape.com/`);
+    const html = new jsdom.JSDOM(axiosResponse.data);
+    
+    const quotes = html.window.document.querySelectorAll('.quote')
+
+    quotes.forEach(quoteElement => {
+
+        const quoteContainer = quoteElement.querySelector('.text');
+        const quote: string = quoteContainer?.textContent ? quoteContainer.textContent : "";
+        
+        const authorContainer = quoteElement.querySelector('.author');
+        const author: string = authorContainer?.textContent ? authorContainer.textContent : "";
+
+        const tagsContainer = quoteElement?.querySelectorAll('.tag');
+        
+        const tags: string[] = [];
+
+        tagsContainer.forEach((tag) => {
+            tags.push(tag?.textContent ? tag.textContent : "")
+        })
+
+        productsList.push({
+            quote,
+            author,
+            tags
+        })
+    })
+
+    response.send(productsList);
+}   
