7e9fb92bc509399d85a3e028473ed78c56fa44a1
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2024-05-14 13:53:50 -0300

    feat: export app

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import scrapeRouter from "./routes/scrape";
+
+const app = express();
+
+app.use('/api/scrape', scrapeRouter);
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Running on port ${PORT}`);
+})
+
+export default app;
